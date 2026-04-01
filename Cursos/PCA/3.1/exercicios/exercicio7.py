9de52e04fe6b3de2ef0ebab52551631aedcfc0d0
Enzo Guinossi <enzowackerguinossi@gmail.com>
2026-04-01 17:28:49 -0300

    feat: adiciona exercício 7 da unidade PCA 3.1

---
+++ Cursos/PCA/3.1/exercicios/exercicio7.py
@@ -0,0 +1,47 @@
+class Pessoa:
+    def __init__(self, nome: str, idade_texto: str) -> None:
+        self.__nome: str = self._tratar_nome(nome)
+        self.__idade: int = self._tratar_idade(idade_texto)
+
+    def _tratar_nome(self, texto: str) -> str:
+        limpo = texto.strip().title()
+        if not limpo:
+            raise ValueError("Nome não pode ser vazio.")
+        return limpo
+
+    def _tratar_idade(self, texto: str) -> int:
+        limpo = texto.strip()
+        if not limpo:
+            raise ValueError("Idade não pode ser vazia.")
+        try:
+            idade = int(limpo)
+            if idade < 0:
+                raise ValueError("Idade não pode ser negativa.")
+            return idade
+        except ValueError:
+            raise ValueError("Idade inválida.")
+
+    @property
+    def nome(self) -> str:
+        return self.__nome
+
+    @property
+    def faixa_etaria(self) -> str:
+        if self.__idade < 10:
+            return "Criança"
+        elif self.__idade < 20:
+            return "Adolescente"
+        return "Adulto"
+
+def main() -> None:
+    try:
+        nome_entrada: str = input("Digite o nome: ")
+        idade_entrada: str = input("Digite a idade: ")
+        
+        pessoa: Pessoa = Pessoa(nome_entrada, idade_entrada)
+        print(f"{pessoa.nome} pertence à faixa etária de {pessoa.faixa_etaria}")
+    except ValueError as e:
+        print(f"Erro: {e}")
+
+if __name__ == "__main__":
+    main()
