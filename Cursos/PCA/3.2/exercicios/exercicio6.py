650d9d5bde129ed50e9b786a6f31d8a3594f1903
Enzo Guinossi <enzowackerguinossi@gmail.com>
2026-04-07 17:09:07 -0300

    feat: adiciona exercício 6 da unidade PCA 3.2

---
+++ Cursos/PCA/3.2/exercicios/exercicio6.py
@@ -0,0 +1,8 @@
+def main():
+    for x in range(1,6):
+        print(f"\nTabuada de {x}\n")
+        for y in range(11):
+            print(f"{x} x {y} = {x*y}")
+
+if __name__ == "__main__":
+    main()
